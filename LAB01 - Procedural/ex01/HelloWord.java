93f6785666c88655d85b436aa15fa4ac5adb38c3
Jhon Waine Mendes Gonçalves <92829457+JhonWaine@users.noreply.github.com>
2025-02-13 00:45:37 -0300

    Tarefa 1

---
+++ LAB01 - Procedural/ex01/HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String nome = "Jhon Waine";
+        String matricula = "12021BCC028";
+        
+        System.out.println("Hello World!");
+        System.out.println("Nome: " + nome);
+        System.out.println("Matrícula: " + matricula);
+    }
+}
